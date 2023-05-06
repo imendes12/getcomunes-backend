768612df8980514afd02b44b7523202a47b0acf2
igormendes <a.igor.mendes@gmail.com>
2023-05-06 14:03:45 -0300

    Add test hello

--- tests/Controller/OlaTest.php
+++ tests/Controller/OlaTest.php
@@ -10,12 +10,25 @@ class OlaTest extends WebTestCase
     /**
      * @Route("/ola")
      */
-    public function testAnnotation()
+    public function testOla()
     {
         $client = static::createClient();
 
         $client->request('GET', '/ola');
 
         $this->assertTrue($client->getResponse()->isSuccessful());
+    }
+
+    /**
+     * @Route("/ola")
+     */
+    public function testHello()
+    {
+        $client = static::createClient();
+
+        $client->request('GET', '/ola');
+
+        $response = $client->getResponse()->getContent();
+        $this->assertStringContainsString('hellow world', $response);
     }
 }
